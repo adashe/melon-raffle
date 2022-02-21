3949108f76de60c868d9c7f36f2ea576d53879de
Andrea Dashe <andrea.r.dashe@gmail.com>
2022-02-21 00:11:24 +0000

    Completed version 2

--- version2/raffle.py
+++ version2/raffle.py
@@ -4,3 +4,23 @@
 
 # Hint: remember to import any functions you need from
 # other files or libraries
+
+import random
+import customers as cstmrs
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = random.choice(customers)
+
+    name = chosen_customer.name
+    email = chosen_customer.email
+
+    print(f"Tell {name} at {email} that they've won")
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    customers = cstmrs.get_customers_from_file("customers.txt")
+    pick_winner(customers)
